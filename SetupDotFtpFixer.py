845fdd6e3222b244e9e1dfeaa73ee5c5295c3276
Mark Olson <mlo@baskerville.org>
2019-01-10 14:41:12 -0500

    Add directory walking code

--- SetupDotFtpFixer.py
+++ SetupDotFtpFixer.py
@@ -1,7 +1,35 @@
 from tkinter import Tk
 from tkinter.filedialog import askdirectory
+import os
 
+def WalkDirectory(dirname):
+    print("Walking "+dirname)
+    if len(dirname) == 0:
+        return
+    if not os.path.isdir(dirname):
+        return
+
+    # First deal with any setup.ftp file present
+    setup=os.path.join(dirname, "setup.ftp")
+    if os.path.exists(setup):
+        print(setup)
+
+    # Now walk any subdirectories
+    subdirs=os.listdir(dirname)
+    subdirs=[os.path.join(dirname, d) for d in subdirs if os.path.isdir(os.path.join(dirname, d))]
+    for subdir in subdirs:
+        WalkDirectory(subdir)
+
+
+# Ask for the root directory
 root = Tk()
 root.filename =  askdirectory(initialdir = ".",title = "Choose the Root Directory")
-print (root.filename)
+dirname=root.filename
 root.withdraw()
+
+if len(dirname) == 0:
+    exit(0)
+
+# Recursively walk the root directory looking for and fixing setup.ftp files.
+WalkDirectory(dirname)
+
